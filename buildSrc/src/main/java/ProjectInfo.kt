d1880555f856461ddcd2f9ea61d7cb34d19ae37f
Quentin Colle <quentin41500@gmail.com>
2021-05-19 14:23:57 -0400

    Fix | Build Issue w/ Detekt + Ktlint (#8)

--- buildSrc/src/main/java/ProjectInfo.kt
+++ buildSrc/src/main/java/ProjectInfo.kt
@@ -16,7 +16,9 @@ object ProjectInfo {
     const val versionCode = versionMajor * 1000000 + versionMinor * 10000 + versionBuild
     const val versionName = "$versionMajor.$versionMinor.$versionBuild"
 
-    const val metaLottieMavenName = "meta-lottie"
-    const val metaLottieMavenGroup = "com.stash"
     const val metaLottieMavenVersion = versionName
+
+    const val metaLottieMavenGroup = "com.stash"
+    const val metaLottiePluginMavenName = "meta-lottie-plugin"
+    const val metaLottieMavenName = "meta-lottie"
 }
